d60167aaf6dfc0f20ff65936a081eb9c22e6eda0
HenryKasai <henrykasai1775@gmail.com>
2020-04-28 18:47:05 -0300

    Resolução de exercício.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Insira determinada medida em metros, e veja a medida em quilômetros e centímetros.");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            string metro = Console.ReadLine();
+            decimal metros = Convert.ToDecimal(metro);
+            string kms = "1000";
+            int km = Convert.ToInt32(kms);
+            decimal resultado = metros / km;
+            string cms = "100";
+            decimal cm = Convert.ToDecimal(cms);
+            decimal resultado2 = metros * cm;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Seu resultado em quilômetros é: " + resultado + "." + "e em centímetros é: " + resultado2 + ".");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Medidas
 Transforme determinada medida em metros para quilômetros e centímetros.
+
+Programa que converte medidas em metro para quilômetro e centímetro.
